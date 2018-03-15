842401025c55da34113586ab9fe49fc819492737
C9Adrian <ruiz.adriansolorio@gmail.com>
2018-03-15 12:50:01 -0700

    initial commit

---
+++ Ast1/Assignment1/Assignment1/main.asm
@@ -0,0 +1,16 @@
+;
+; DA2_1.asm
+;
+; Created: 2/26/2018 11:30:48 AM
+; Author : Adrian
+;
+
+
+; Replace with your application code
+
+.CSEG;				code sgement
+
+
+start:
+    inc r16
+    rjmp start
